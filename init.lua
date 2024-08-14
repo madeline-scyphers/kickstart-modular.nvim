8194a7cc166b70fe4aaf69df8a3aa4215cc0e503
Madeline Scyphers <madelinescyphers@gmail.com>
2024-08-14 18:24:24 -0400

    highlight nextflow as groovy

--- init.lua
+++ init.lua
@@ -105,5 +105,12 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+
+vim.filetype.add({
+  extension = {
+    nf = 'groovy',
+  },
+})
+vim.treesitter.language.register('groovy', 'nf')
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
